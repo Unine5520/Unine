3b35efb10ff753bd0de29c103df9faba01e7b8a6
Unine5520 <unineaone@gmail.com>
2026-08-23 10:59:54 +0630

    Update setting.js

--- me/setting/setting.js
+++ me/setting/setting.js
@@ -21,3 +21,87 @@ if(logoutButton){
 
 
 }
+
+console.log("setting.js loaded");
+
+
+const logoutButton =
+document.getElementById(
+    "logoutButton"
+);
+
+
+
+if(logoutButton){
+
+
+    logoutButton.onclick =
+    async()=>{
+
+
+        console.log(
+            "Logout button clicked"
+        );
+
+
+
+        const confirmLogout =
+        confirm(
+            "Are you sure you want to logout?"
+        );
+
+
+
+        if(!confirmLogout){
+
+            return;
+
+        }
+
+
+
+
+        const {
+            error
+        } =
+        await supabaseClient
+        .auth
+        .signOut();
+
+
+
+
+        if(error){
+
+
+            console.error(
+                "Logout error:",
+                error
+            );
+
+
+            return;
+
+
+        }
+
+
+
+
+
+        console.log(
+            "Logout successful"
+        );
+
+
+
+        switchPage(
+            "home"
+        );
+
+
+
+    };
+
+
+}
