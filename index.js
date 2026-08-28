d3af8c2b20e7c296061f4b8a8452b9661f33eb39
Unine5520 <unineaone@gmail.com>
2026-08-28 07:29:52 +0630

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,10 @@
 ========================================= */
 
 const SUPABASE_URL =
-"https://moufqvgakqqozybedisj.supabase.co";
+"---------";
 
 const SUPABASE_PUBLISHABLE_KEY =
-"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1vdWZxdmdha3Fxb3p5YmVkaXNqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODczNzI0NjQsImV4cCI6MjEwMjk0ODQ2NH0.LjMk0ZDmImS4NYezx6Xp6FbUxVrH_esroZXzXBWkiVc";
+"--------";
 
 
 /* =========================================
