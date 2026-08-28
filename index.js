0a909a982476f8d7c7ef93f26b36072fd1c5ddca
Unine5520 <unineaone@gmail.com>
2026-08-28 13:57:29 +0630

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
-/* =========================================
-   SUPABASE CONFIG
-========================================= */
-
-const SUPABASE_URL =
-"---------";
-
-const SUPABASE_PUBLISHABLE_KEY =
-"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxheXpjZ2t0Z3RycXZzZ3h3d3ljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc4OTIxNDUsImV4cCI6MjEwMzQ2ODE0NX0.A-9DppiKBSLkjsFEW5fT1WZuWd1b4f_bFp7iTdm9t6E";
-
-/* =========================================
-   SUPABASE CLIENT
-========================================= */
-
-const supabase = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_PUBLISHABLE_KEY
-);
-
-
 /* ======== PAGE NAVIGATION ======== */
 /* ======== PAGE NAVIGATION ======== */
 /* ======== PAGE NAVIGATION ======== */
