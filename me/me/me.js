514717093b1696633e2164237992301d6ce33cc2
Unine5520 <unineaone@gmail.com>
2026-08-29 16:32:35 +0630

    Update me.js

--- me/me/me.js
+++ me/me/me.js
@@ -1,1 +1,38 @@
+<div
+    id="logoutModal"
+    class="logout-modal hidden"
+>
 
+
+<div class="logout-box">
+
+
+<h3>
+    Confirm Logout?
+</h3>
+
+
+<div class="logout-actions">
+
+
+<button
+    id="logoutConfirm"
+>
+    Confirm
+</button>
+
+
+<button
+    id="logoutCancel"
+>
+    Cancel
+</button>
+
+
+</div>
+
+
+</div>
+
+
+</div>
