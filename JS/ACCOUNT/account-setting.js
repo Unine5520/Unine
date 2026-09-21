55fadc017ea2f4176276afc8e1a044f0ac55bc14
Unine5520 <unineaone@gmail.com>
2026-09-21 22:28:04 +0630

    Update account-setting.js

--- JS/ACCOUNT/account-setting.js
+++ JS/ACCOUNT/account-setting.js
@@ -1,1 +1,53 @@
+/* =========================
+   ACCOUNT SETTING
+========================= */
 
+const accountSetting =
+  document.getElementById(
+    "U9-account-setting"
+  );
+
+
+const userButton =
+  document.getElementById(
+    "U9-page-header-user"
+  );
+
+
+/* =========================
+   OPEN ACCOUNT SETTING
+========================= */
+
+userButton.addEventListener(
+  "click",
+  () => {
+
+    accountSetting.style.display =
+      "block";
+
+  }
+);
+
+
+/* =========================
+   LOGOUT BUTTON
+========================= */
+
+const accountSettingLogout =
+  document.getElementById(
+    "U9-account-setting-5"
+  );
+
+
+accountSettingLogout.addEventListener(
+  "click",
+  () => {
+
+    accountSetting.style.display =
+      "none";
+
+
+    openLogoutConfirm();
+
+  }
+);
