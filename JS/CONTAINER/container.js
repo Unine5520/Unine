167fa83130526ae14d5ca1b7d3e1a9263363d0f7
Unine5520 <unineaone@gmail.com>
2026-09-23 17:31:07 +0630

    Update container.js

--- JS/CONTAINER/container.js
+++ JS/CONTAINER/container.js
@@ -1,1 +1,88 @@
 
+/* =========================
+   SEARCH
+========================= */
+
+const searchInput =
+  document.getElementById(
+    "U9-page-container-search-input"
+  );
+
+
+const searchButton =
+  document.getElementById(
+    "U9-page-container-search-button"
+  );
+
+
+/* =========================
+   CHECK SEARCH INPUT
+========================= */
+
+function updateSearchButton() {
+
+  if (
+    searchInput.value.trim() === ""
+  ) {
+
+    searchButton.style.backgroundColor =
+      "#abacb2";
+
+    return;
+
+  }
+
+
+  searchButton.style.backgroundColor =
+    "#30bff1";
+
+}
+
+
+/* =========================
+   INPUT
+========================= */
+
+searchInput.addEventListener(
+  "input",
+  updateSearchButton
+);
+
+
+/* =========================
+   SEARCH BUTTON
+========================= */
+
+searchButton.addEventListener(
+  "click",
+  function () {
+
+    const keyword =
+      searchInput.value.trim();
+
+
+    /* Empty */
+
+    if (keyword === "") {
+
+      return;
+
+    }
+
+
+    /* Search */
+
+    console.log(
+      "Search:",
+      keyword
+    );
+
+  }
+);
+
+
+/* =========================
+   INITIAL STATE
+========================= */
+
+updateSearchButton();
