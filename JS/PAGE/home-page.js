ee6c98982e72a3e56300710a273b4c4002e0847d
Unine5520 <unineaone@gmail.com>
2026-09-24 17:38:33 +0630

    Update home-page.js

--- JS/PAGE/home-page.js
+++ JS/PAGE/home-page.js
@@ -1,1 +1,96 @@
+/* =========================
+   HOME PAGE PRODUCTS
+========================= */
 
+const product1 =
+  document.getElementById(
+    "U9-page-home-product-1"
+  );
+
+
+const product2 =
+  document.getElementById(
+    "U9-page-home-product-2"
+  );
+
+
+const product1Image =
+  product1.querySelector(
+    "img"
+  );
+
+
+const product2Image =
+  product2.querySelector(
+    "img"
+  );
+
+
+/* =========================
+   PRODUCT 1
+========================= */
+
+function loadProduct1() {
+
+  if (
+    product1Image.complete &&
+    product1Image.naturalWidth > 0
+  ) {
+
+    product1.classList.add(
+      "loaded"
+    );
+
+  }
+
+}
+
+
+product1Image.addEventListener(
+  "load",
+  function () {
+
+    product1.classList.add(
+      "loaded"
+    );
+
+  }
+);
+
+
+loadProduct1();
+
+
+/* =========================
+   PRODUCT 2
+========================= */
+
+function loadProduct2() {
+
+  if (
+    product2Image.complete &&
+    product2Image.naturalWidth > 0
+  ) {
+
+    product2.classList.add(
+      "loaded"
+    );
+
+  }
+
+}
+
+
+product2Image.addEventListener(
+  "load",
+  function () {
+
+    product2.classList.add(
+      "loaded"
+    );
+
+  }
+);
+
+
+loadProduct2();
